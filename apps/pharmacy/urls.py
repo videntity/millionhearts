f2998e96bb4d024039be833a3d9f995c036aaa74
Alan Viars <aviars@videntiity.com>
2012-10-03 17:01:09 -0400

    passing patient id through the find pharmacy steps

--- apps/pharmacy/urls.py
+++ apps/pharmacy/urls.py
@@ -10,4 +10,9 @@ urlpatterns = patterns('',
     url(r'find', find_pharmacy,  name="find_pharmacy"),
     url(r'show', show_pharmacy,  name="show_pharmacy"),
     
+    
+    url(r'find/(?P<patient_id>\S+)', find_pharmacy,  name="find_pharmacy"),
+    url(r'show/(?P<patient_id>\S+)', show_pharmacy,  name="show_pharmacy"),
+    
+    
     )
--- apps/pharmacy/views.py
+++ apps/pharmacy/views.py
@@ -12,13 +12,19 @@ from django.core.urlresolvers import reverse
 from django.contrib import messages
 from ..utils import get_latest_object_or_404
 from forms import *
+from ..riskassessments.models import ArchimedesRiskAssessment
 
-def show_pharmacy(request):
+def show_pharmacy(request, patient_id=None):
     pass
 
 
-def find_pharmacy(request):
+def find_pharmacy(request, patient_id=None):
     
+    if patient_id:
+        patient = get_latest_object_or_404(ArchimedesRiskAssessment,
+                                       patient_id=patient_id)
+    else:
+        patient=None
     if request.method == 'POST':
         form =  FindPharmacyForm(request.POST)
         
@@ -27,11 +33,14 @@ def find_pharmacy(request):
             
             return render_to_response("pharmacy/show.html",
                               RequestContext(request,
-                                             {'result': result,}))   
+                                             {'result': result,
+                                              'patient':patient,
+                                              }))   
         else:
             return render_to_response("pharmacy/find.html",
                               RequestContext(request,
-                                             {'form': form,}))
+                                             {'form': form,
+                                              'patient':patient,}))
     
     
     
@@ -39,5 +48,6 @@ def find_pharmacy(request):
     return render_to_response("generic/bootstrapform.html",
                              {'name': "Tell us some basic information",
                               'submit_button_text': "Go On",
+                              "patient": patient,
                               'form': FindPharmacyForm(),},
                               context_instance = RequestContext(request))
